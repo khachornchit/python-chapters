a9c0300597de4f39aa28923a74a75db309724b2e
Chorn <chorndev01@gmail.com>
2023-11-04 13:17:22 +0700

    update

--- readDataUrl.py
+++ readDataUrl.py
@@ -94,59 +94,52 @@ def loadDataToMovieDict(urlRawData):
 
 
 def searchQuery(query, movies):
-    words = query.lower().split()
-    n = len(words)
 
-    stage1s = []
-    stage2s = []
-    stage3s = []
-
-    # Search by keywords
-    for i in range(len(words)):
+    def getKeywords(query):
         keywords = []
-        keyword1 = ' '.join(words[i:n])
-        keywords.append(keyword1)
+        words = query.split()
+        n = len(words)
 
-        # Search movies stage1
+        for i in range(len(words)):
+            keyword1 = ' '.join(words[i:n])
+            keywords.append(keyword1)
+
+            if (i > 0):
+                keyword2 = ' '.join(words[0:n-i])
+                keywords.append(keyword2)
+
+            if (i > 0 and i < n-1):
+                keyword3 = ' '.join(words[i:n-1])
+                keywords.append(keyword3)
+        return keywords
+
+    def searchByKeyword(keyword):
+        results = []
         for movie in movies:
             titles = movie['title'].lower()
-            if (keyword1 in titles):
-                stage1s.append(movie)
+            if (keyword in titles):
+                results.append(movie)
+        return results
 
-        if (len(stage1s) > 0):
-            return [keywords, stage1s]
+    def main():
+        keywords = getKeywords(query)
+        for keyword in keywords:
+            results = searchByKeyword(keyword)
+            if (len(results) > 0):
+                return [keyword, results]
 
-        keywords = []
-        if (i > 0):
-            keyword2 = ' '.join(words[0:n-i])
-            keywords.append(keyword2)
+        keyword = keywords
+        return [keywords, results]
 
-            # Search movies stage2
-            for movie in movies:
-                titles = movie['title'].lower()
-                if (keyword2 in titles):
-                    stage2s.append(movie)
-            if (len(stage2s) > 0):
-                return [keywords, stage2s]
-
-        keywords = []
-        if (i > 0 and i < n-1):
-            keyword3 = ' '.join(words[i:n-1])
-            keywords.append(keyword3)
-
-            # Search movies stage3
-            for movie in movies:
-                titles = movie['title'].lower()
-                if (keyword3 in titles):
-                    stage3s.append(movie)
-
-        return [keywords, stage3s]
+    return main()
 
 
 if __name__ == "__main__":
     urlRawData = 'https://raw.githubusercontent.com/sothornin/file/main/movies_2010_2013.json'
     [movieWithIds, movieNoIds] = loadDataToMovieDict(urlRawData)
-    [keywords, movies] = searchQuery("happy", movieNoIds)
+    [keyword, results] = searchQuery("my way", movieNoIds)
 
-    for movie in movies:
+    print(keyword)
+
+    for movie in results:
         print(movie)
