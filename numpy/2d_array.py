d5048ce348206958ca3207eaffc7e502e1f0a3f4
Chorn <chorndev01@gmail.com>
2023-11-11 19:37:30 +0700

    update

---
+++ numpy/2d_array.py
@@ -0,0 +1,18 @@
+#! D:\python-projects\python_chapters\numpy_env\Scripts\python.exe
+
+import numpy as np
+
+def create_5x6():
+    return np.array([
+        [4,0,4,0,5],
+        [5,3,2,8,4],
+        [5,7,0,0,5],
+        [3,0,1,2,8],
+        [0,7,2,3,0],
+        [0,7,2,3,0],
+        [0,6,3,3,0],
+    ])
+    
+if __name__ == "__main__":
+    array = create_5x6()
+    print(array)
